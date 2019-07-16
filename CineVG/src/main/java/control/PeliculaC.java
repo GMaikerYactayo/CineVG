67d36278c02b1e87941cce201182d713c606513f
RobertoEBM <roberto.borjas@vallegrande.edu.pe>
2019-07-16 13:02:33 -0500

    Dashboard (Cantidad de películas por género)

--- CineVG/src/main/java/control/PeliculaC.java
+++ CineVG/src/main/java/control/PeliculaC.java
@@ -1,14 +1,22 @@
 package control;
 
+import dao.DashboardImpl;
 import dao.PeliculaImpl;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.sql.SQLException;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import modelo.NPelicula;
 import modelo.Pelicula;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.BarChartSeries;
+import org.primefaces.model.chart.ChartSeries;
 
 @Named(value = "peliculaC")
 @SessionScoped
@@ -17,11 +25,14 @@ public class PeliculaC implements Serializable {
     private Pelicula pelicula = new Pelicula();
     private Pelicula selectedPelicula;
     private List<Pelicula> listadoPel;
+    private BarChartModel barra;
+    private List<NPelicula> NPelicula;
 
     @PostConstruct
     public void iniciar() {
         try {
             listar();
+            lstCantPeli();
         } catch (Exception e) {
         }
 
@@ -35,7 +46,43 @@ public class PeliculaC implements Serializable {
         }
 
     }
-
+    
+     public void lstCantPeli() throws Exception {
+        DashboardImpl dao;
+        try {
+            dao = new DashboardImpl();
+            NPelicula = dao.listarCantPeli();
+            createBarras();
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
+     
+         private void createBarras() {
+        try {
+            barra = new BarChartModel();
+            for (int i = 0; i < NPelicula.size(); i++) {
+                ChartSeries serie = new BarChartSeries();
+                serie.setLabel(NPelicula.get(i).getIDPEL());
+                serie.set(NPelicula.get(i).getCOUNTPEL(), NPelicula.get(i).getCOUNTPEL());
+                barra.addSeries(serie);
+                barra.setLegendPosition("ne");
+            }
+            Axis xAxis = barra.getAxis(AxisType.X);
+            xAxis.setLabel("CINE VG");
+            barra = getBarra();
+            barra.setTitle("NÚMERO DE PELÍCULAS POR GÉNERO");
+            barra.setAnimate(true);
+            
+            Axis yAxis = barra.getAxis(AxisType.Y);
+            yAxis.setMax(30);
+        } catch (Exception e) {
+            System.out.println("error" +e.getMessage());
+        }
+    }
+    
+    
+    
     public void registrarPelicula() throws Exception {
         PeliculaImpl dao;
         try {
@@ -71,7 +118,7 @@ public class PeliculaC implements Serializable {
             FacesContext.getCurrentInstance().addMessage(null,
                     new FacesMessage(FacesMessage.SEVERITY_INFO, "Eliminando", "completado..."));
         } catch (Exception e) {
-            e.printStackTrace();
+            throw e;
         }
     }
 
@@ -108,5 +155,26 @@ public class PeliculaC implements Serializable {
     public void setSelectedPelicula(Pelicula selectedPelicula) {
         this.selectedPelicula = selectedPelicula;
     }
+
+    public BarChartModel getBarra() {
+        return barra;
+    }
+
+    public void setBarra(BarChartModel barra) {
+        this.barra = barra;
+    }
+
+    public List<NPelicula> getNPelicula() {
+        return NPelicula;
+    }
+
+    public void setNPelicula(List<NPelicula> NPelicula) {
+        this.NPelicula = NPelicula;
+    }
+
+
+
+    
+    
 
 }
---
+++ CineVG/src/main/java/modelo/NPelicula.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public class NPelicula {
+    private String IDPEL;
+    private int COUNTPEL;
+
+    public String getIDPEL() {
+        return IDPEL;
+    }
+
+    public void setIDPEL(String IDPEL) {
+        this.IDPEL = IDPEL;
+    } 
+
+    public int getCOUNTPEL() {
+        return COUNTPEL;
+    }
+
+    public void setCOUNTPEL(int COUNTPEL) {
+        this.COUNTPEL = COUNTPEL;
+    }
+
+
+    }
+
+
+    
+    
+    
